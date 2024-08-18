eaaade8f681e13b5c7dfe725929d5bab52fc4c68
Jorge Verdugo <jorgeverdugoch@gmail.com>
2024-08-17 21:09:26 -0400

    feat: Docker

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+FROM node:18-slim
+
+# Installs latest Chromium (103) package.
+RUN apt update -qq \
+    && apt install -qq -y --no-install-recommends \
+    curl \
+    git \
+    gnupg \
+    libgconf-2-4 \
+    libxss1 \
+    libxtst6 \
+    g++ \
+    build-essential \
+    chromium \
+    chromium-sandbox \
+    dumb-init \
+    fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst \
+    && rm -rf /var/lib/apt/lists/* \
+    && rm -rf /src/*.deb
+
+# Tell Puppeteer to skip installing Chrome. We'll be using the installed package.
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+ENV CHROME_PATH=/usr/bin/chromium
+ENV PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
+
+WORKDIR /home/pptruser
+
+RUN npm install puppeteer
+
+# Add user so we don't need --no-sandbox.
+RUN groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
+    && mkdir -p /home/pptruser/Downloads \
+    && chown -R pptruser:pptruser /home/pptruser
+
+COPY . /home/pptruser
+
+RUN npm install
+
+RUN npx tsc
+
+RUN chown -R pptruser:pptruser /home/pptruser/dist \
+    && chown -R pptruser:pptruser /home/pptruser
+
+EXPOSE 3000
+
+USER pptruser
+
+CMD [ "node", "dist/index.js" ]
--- src/index.ts
+++ src/index.ts
@@ -1,120 +1,171 @@
-import { Builder, Browser, By, until, WebElement } from "selenium-webdriver";
+import moment from "moment-timezone";
+import puppeteer, { Page } from "puppeteer";
 
-// 1. Create a new Builder instance
-// 2. Connect to the browser
-// 3. Connect to pokemon web
-// 4. Iterate every 10 minutes?
-// 5. check for May 15 - 17 spots
-// 6. If spots are available, take spot
-// 6b. Priority for seating A
-// Return if not available
-// 7. Fill input
-// 8. Await input on access code?
-const NUM_PEOPLE = 3;
-const ENVIRONMENT = "DEV" : "PROD";
+const DAYS_BEFORE = 30;
+const OPEN_TIME = "18:00";
 
-async function run() {
-  const driver = await new Builder().forBrowser(Browser.CHROME).build();
-  try {
-    await driver.get("https://reserve.pokemon-cafe.jp/reserve/step1");
-    // calendar page
-    await driver.wait(
-      until.elementLocated(By.xpath("//select[@name='guest']")),
-      1000
-    );
-    await driver.findElement(By.xpath("//select[@name='guest']")).click();
-    await driver
-      .findElement(By.xpath(`//option[@value=${NUM_PEOPLE}]`))
-      .click();
+async function selectMonth(page: Page, dateToBook: string) {
+  const calendarPagerSelector = '#step2-form a.calendar-pager';
+  await page.waitForSelector(calendarPagerSelector);
 
-    await driver.wait(
-      until.elementLocated(By.xpath(`//*[contains(text(), "次の月を見る")]`)),
-      1000
-    );
-    let available = [];
-    let date;
-    // const dayInterval = setInterval(async () => {
-    //   await driver
-    //   .findElement(By.xpath(`//*[contains(text(), "次の月を見る")]`))
-    //   .click();
+  const currentMonthYearSelector = '#step2-form h3';
+  await page.waitForSelector(currentMonthYearSelector);
+  const currentMonthYearElement = await page.$(currentMonthYearSelector);
+  const currentMonthYear = await page.evaluate((element) => element.textContent, currentMonthYearElement);
 
-    // available = await driver.findElements(
-    //   By.xpath('//li[@class="calendar-day-cell available"]') // TODO: Change to available
-    // );
+  const currentMonthYearParts = currentMonthYear.split('年');
+  const currentYear = parseInt(currentMonthYearParts[0]);
+  const currentMonth = parseInt(currentMonthYearParts[1].replace('月', ''));
 
-    // if (available.length !== 0) {
-    //   clearInterval(dayInterval);
-    //   console.log("refreshed");
-    // }
-    // }, 1000);
-    await driver
-      .findElement(By.xpath(`//*[contains(text(), "次の月を見る")]`))
-      .click();
+  const dateToBookParts = dateToBook.split('-');
+  const yearToBook = parseInt(dateToBookParts[0]);
+  const monthToBook = parseInt(dateToBookParts[1]);
 
-    // while (available.length === 0) {
+  const monthDiff = (yearToBook - currentYear) * 12 + (monthToBook - currentMonth);
+ 
+  const calendarPagerElements = await page.$$(calendarPagerSelector);
+  const prevMonthElement = calendarPagerElements[0];
+  const nextMonthElement = calendarPagerElements[1];
 
-    available = await driver.findElements(
-      By.xpath('//li[@class="calendar-day-cell not-available"]') // TODO: Change to available
-    );
-
-    // if (available.length === 0) {
-    //   driver.navigate().refresh();
-    //   console.log("refreshed");
-    // }
-    // }
-    let i = 0;
-    let c = true;
-    while (i < available.length && c) {
-      const text = await available[i].getText();
-      if (text.includes("15")) {
-        date = 15;
-        c = false;
-      } else if (text.includes("16")) {
-        date = 16;
-        c = false;
-      } else if (text.includes("17")) {
-        date = 17;
-        c = false;
-      }
-      i++;
+  if (monthDiff > 0) {
+    for (let i = 0; i < monthDiff; i++) {
+      await nextMonthElement.click();
     }
-    await driver
-      .findElement(By.xpath(`//*[contains(text(), ${date})]`))
-      .click();
-    await driver.findElement(By.id("submit_button")).click();
-
-    // time select page
-    await driver.wait(until.elementLocated(By.className("time-cell")), 1000);
-    let openTimes: WebElement[] = await driver.findElements(
-      By.xpath('//div[@class="level full"]')
-    ); // TODO: Change to available after finding the value
-
-    // priority for seating A
-    let j = 0;
-    if (openTimes.length === 0) {
-      driver.navigate().refresh();
+  } else if (monthDiff < 0) {
+    for (let i = 0; i < monthDiff * -1; i++) {
+      await prevMonthElement.click();
     }
-    while (j < openTimes.length && c) {
-      const text = await openTimes[j].getText();
-      if (text.includes("A")) {
-        await openTimes[j].click();
-        c = false;
-      } else if (text.includes("C")) {
-        await openTimes[j].click();
-        c = false;
-      } else if (text.includes("B")) {
-        await openTimes[j].click();
-        c = false;
-      }
-      console.log(text);
-      j++;
-    }
-
-    // if the list is empty, refresh
-  } catch {
-    console.log("Error");
-  } finally {
-    // await driver.quit();
   }
 }
+
+async function selectDate(page: Page, dateToBook: string | undefined) {
+  if (!dateToBook) {
+    const rawDate =  moment().tz('Asia/Tokyo').add(DAYS_BEFORE - 1, 'days');
+    let dateOpened = rawDate.format('YYYY-MM-DD');
+    console.log(rawDate);
+
+    if (rawDate.hours() >= parseInt(OPEN_TIME.split(':')[0])) {
+      dateOpened = rawDate.add(1, 'days').format('YYYY-MM-DD');
+    }
+    await selectMonth(page, dateOpened);
+  } else {
+    await selectMonth(page, dateToBook);
+  }
+
+  
+  let dayToBook = null;
+  if (dateToBook) {
+    const dateParts = dateToBook.split('-');
+    dayToBook = parseInt(dateParts[2]);
+  }
+  
+  const dayToBookData = await page.evaluate((dayToBook) => {
+    try {
+      const calendarDaySelector = '#step2-form li';
+    const calendarDayElements  = Array.from(document.querySelectorAll(calendarDaySelector));
+
+    const filteredDays = calendarDayElements.filter((li) => {
+      if (dayToBook) {
+        return li.childNodes[0].childNodes[0].nodeType == 3 && (li.childNodes[0].childNodes[0] as Text).data == dayToBook.toString();
+      } else {
+        const status = li.childNodes[0].childNodes[2].textContent.replace('(', '').replace(')', '');
+        const available = status != 'Full' && status != 'N/A';
+        return available;
+      }
+    });
+
+    let dayToBookLiElement = filteredDays.length > 0 ? filteredDays[filteredDays.length - 1] : null
+
+    if (dayToBookLiElement == null) {
+      return null;
+    }
+
+    const number = (dayToBookLiElement.childNodes[0].childNodes[0] as Text).data;
+    const statusJapanese = dayToBookLiElement.childNodes[0].childNodes[1].textContent;
+    const status = dayToBookLiElement.childNodes[0].childNodes[2].textContent.replace('(', '').replace(')', '');
+    const available = status != 'Full' && status != 'N/A';
+
+    return {
+      number,
+      statusJapanese,
+      status,
+      available,
+      raw: dayToBookLiElement.textContent,
+    }
+    } catch (error) {
+      return error;
+    }
+  }, dayToBook);
+
+  return dayToBookData;
+}
+
+async function createBooking(city: string, numOfGuests: number, dateToBook: string | undefined) {
+
+  let url: string;
+
+  if (city == "Tokyo") {
+    url = "https://reserve.pokemon-cafe.jp/"
+  } else if ( city == "Osaka") {
+    url = "https://osaka.pokemon-cafe.jp/"
+  } else {
+    throw new Error("City not supported");
+  }
+
+  if (numOfGuests < 1) {
+    throw new Error("Number of guests must be at least 1");
+  }
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+
+  const page = await browser.newPage();
+  await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36");
+  
+  await page.goto(url + "reserve/step1");
+
+  const selectNumGuestSelector = 'select[name=guest]';
+  await page.waitForSelector(selectNumGuestSelector);
+  await page.select(selectNumGuestSelector, numOfGuests.toString());
+
+  console.log("Waiting for calendar...");
+
+  const calendarSelector = 'input[name=date]';
+  await page.waitForSelector(calendarSelector);
+
+  await page.screenshot({path: 'calendar_screenshot.png'});
+
+  console.log("Selecting date...");
+
+  const dateToBookData = await selectDate(page, dateToBook);
+
+  console.log("Date selected!");
+
+   if (dateToBookData == null) {
+    if (dateToBook) {
+      throw new Error("Date not found");
+    } else {
+      throw new Error("No available date");
+    }
+  }
+
+  if (!dateToBookData.available) {
+    throw new Error("Date not available");
+  }
+
+/*   try {
+    const currentMonthYear = await page.evaluate((element) => element.click(), dateToBookData.element);
+  } catch (error) {
+    console.error(error);
+  } */
+
+}
+
+async function run() {
+
+  await createBooking("Tokyo", 4, null);
+}
+
 run();
